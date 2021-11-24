76bc21bff112331937dbc6731128398a1b9b039c
Mark <mark28ten@gmail.com>
2021-11-24 09:11:28 -0800

    get test

--- index.js
+++ index.js
@@ -8,23 +8,49 @@ exports.handler = async (event) => {
   console.log('EVENT', event);
 
   try {
-    const { name, phone } = JSON.parse(event.body);
-    const id = uuid();
 
-    const record = new peopleModel({ id, name });
-    console.log('RECORD', record);
+    // either there is or is not an id
+    const id = event.pathParameters && event.pathParameters.id;
 
-    const data = await record.save();
+    let data;
 
+    if (id) {
+      const list = await peopleModel.query('id').eq(id).exec();
+      console.log('LIST', list);
+      data = list[0];
+    } else {
+      data = await peopleModel.scan().exec();
+    }
     return {
       statusCode: 200,
       body: JSON.stringify(data),
-    };
+    }
 
   } catch (err) {
     return {
       statusCode: 500,
-      response: err.message,
-    };
+      response: err.message
+    }
+  }
+
+  // try {
+  //   const { name, phone } = JSON.parse(event.body);
+  //   const id = uuid();
+
+  //   const record = new peopleModel({ id, name });
+  //   console.log('RECORD', record);
+
+  //   const data = await record.save();
+
+  //   return {
+  //     statusCode: 200,
+  //     body: JSON.stringify(data),
+  //   };
+
+  // } catch (err) {
+  //   return {
+  //     statusCode: 500,
+  //     response: err.message,
+  //   };
   }
 };
